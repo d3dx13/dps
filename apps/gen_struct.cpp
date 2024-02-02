8a07b87109d3b1f034e665bd0b4d40825d99995e
d3dx13 <d3dx13@veresk.tech>
2024-02-02 21:00:30 +0300

    allocate arrays from buffer

--- apps/gen_struct.cpp
+++ apps/gen_struct.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
-#include <unordered_map>
-#include <variant>
-#include <cstring>
-#include <any>
+#include <string>
+#include <bitset>
+#include <climits>
+#include <type_traits>
 
 struct message {
-    std::uint16_t a[2];
-    std::uint32_t b[2];
+    std::uint16_t *a;
+    std::uint32_t *b;
+    std::uint8_t *image;
+    float fv;
 };
 
-int main() {
-    std::unordered_map<std::string, std::variant<std::uint8_t, double, float, std::string>> umap;
-    umap["a"] = (double) 1231.1231;
-    umap["b"] = (float) 455.555;
-    umap["s"] = std::string("123123121321");
-    umap["i8"] = (std::uint8_t) 23;
+#define IS_INTEGRAL(T) typename std::enable_if< std::is_integral<T>::value >::type* = 0
 
-    char buffer[16] = {0};
-    for (char ch : buffer){
-        std::cout << "ch: " << (int)ch << "\n";
+template<class T>
+std::string integral_to_binary_string(T byte, IS_INTEGRAL(T))
+{
+    std::bitset<sizeof(T) * CHAR_BIT> bs(byte);
+    return bs.to_string();
+}
+
+int main() {
+    char buffer[32];
+    for (char ch: buffer) {
+        std::cout << "ch: " << (int) ch << "\n";
     }
     std::cout << "\n";
 
@@ -28,29 +33,38 @@ int main() {
     //    std::cout << it.first << " : " << std::get<std::uint8_t>(it.second) << "\n";
     //}
 
-    auto *pA = static_cast<message *>(static_cast<void*>(&buffer[2]));
+    message msg{};
+    msg.a = new(buffer + 0) std::uint16_t[2];
+    msg.b = new(buffer + 8) std::uint32_t[2];
+    msg.image = new(buffer + 16) std::uint8_t[2];
 
-    std::cout << "a 0 = " << pA->a[0] << "\n";
-    std::cout << "b 0 = " << pA->b[0] << "\n";
-    std::cout << "a 1 = " << pA->a[1] << "\n";
-    std::cout << "b 1 = " << pA->b[1] << "\n";
-    std::cout << "size: " << sizeof(pA) << "\n";
-    for (char ch : buffer){
-        std::cout << "ch: " << (int)ch << "\n";
+    // auto *pA = static_cast<message *>(static_cast<void *>(&buffer[2]));
+
+    std::cout << "a 0 = " << msg.a[0] << "\n";
+    std::cout << "b 0 = " << msg.b[0] << "\n";
+    std::cout << "a 1 = " << msg.a[1] << "\n";
+    std::cout << "b 1 = " << msg.b[1] << "\n";
+    std::cout << "bt 0 = " << (unsigned int) msg.image[0] << "\n";
+    std::cout << "bt 1 = " << (unsigned int) msg.image[1] << "\n";
+    std::cout << "size: " << sizeof(message) << "\n";
+    for (char ch: buffer) {
+        std::cout << "ch: " << integral_to_binary_string(ch) << "\n";
     }
     std::cout << "\n";
 
-    pA->a[0] = 12312;
-    pA->a[1] = 2222;
-    pA->b[0] = 1312312312;
-    pA->b[1] = 222322223;
-    std::cout << "a 0 = " << pA->a[0] << "\n";
-    std::cout << "b 0 = " << pA->b[0] << "\n";
-    std::cout << "a 1 = " << pA->a[1] << "\n";
-    std::cout << "b 1 = " << pA->b[1] << "\n";
-    std::cout << "size: " << sizeof(pA) << "\n";
-    for (char ch : buffer){
-        std::cout << "ch: " << (unsigned int)ch << "\n";
+    msg.a[0] = 12312;
+    msg.a[1] = 2222;
+    msg.b[0] = 1312312312;
+    msg.b[1] = 222322223;
+    std::cout << "a 0 = " << msg.a[0] << "\n";
+    std::cout << "b 0 = " << msg.b[0] << "\n";
+    std::cout << "a 1 = " << msg.a[1] << "\n";
+    std::cout << "b 1 = " << msg.b[1] << "\n";
+    std::cout << "bt 0 = " << (unsigned int) msg.image[0] << "\n";
+    std::cout << "bt 1 = " << (unsigned int) msg.image[1] << "\n";
+    std::cout << "size: " << sizeof(message) << "\n";
+    for (char ch: buffer) {
+        std::cout << "ch: " << integral_to_binary_string(ch) << "\n";
     }
     std::cout << "\n";
 
