b0079fd3c5d0d3208cf9a6c4fc261a2a2cf423c8
d3dx13 <zhidkov2846@gmail.com>
2024-02-15 13:14:18 +0300

    Деструктор аллокатора и комментарии

--- src/allocator.cpp
+++ src/allocator.cpp
@@ -5,9 +5,12 @@
 #include <vector>
 
 Allocator::Allocator(Allocator::memory memory_space) {
+    // Trying to find dma_heap
     if (!std::filesystem::exists(this->memory_path[memory_space])) {
         throw exceptions::DmaBufferNotFound(this->memory_path[memory_space]);
     }
+
+    // Trying to open dma_heap
     this->memory_space = memory_space;
     this->dma_heap_fd = dmabuf_heap_open(this->memory_path[this->memory_space]);
     if (this->dma_heap_fd < 0) {
@@ -16,8 +19,10 @@ Allocator::Allocator(Allocator::memory memory_space) {
 }
 
 Allocator::Allocator() {
-    bool valid_memory_path_found = false;
-    std::vector<std::string> memory_path_exists;
+    bool valid_memory_path_found = false; // Flag that a file descriptor was successfully created on dma_heap
+    std::vector<std::string> memory_path_exists; // List of dma_heap that exist, but to which there are no access rights
+
+    // Trying to open first accessible dma_heap
     for (std::pair<memory, std::string> mem: this->memory_path) {
         if (!std::filesystem::exists(mem.second)) {
             continue;
@@ -31,7 +36,8 @@ Allocator::Allocator() {
         valid_memory_path_found = true;
         break;
     }
-    if (!valid_memory_path_found) {
+
+    if (!valid_memory_path_found) { // Handle scenarios when no dma_heap could be opened
         if (memory_path_exists.empty()) {
             throw exceptions::DmaBufferNotFound("No One");
         } else {
@@ -41,13 +47,16 @@ Allocator::Allocator() {
             }
             throw exceptions::DmaBufferNoUserRights(memory_path_exists[0]);
         }
-    } else {
+    } else { // Successful opening of the first dma_heap
         std::cout << "DMA Buffer Auto Found : " << this->memory_path[this->memory_space] << "\n";;
     }
 }
 
 Allocator::~Allocator() {
-
+    // Closing dma_heap
+    if (this->dma_heap_fd >= 0) {
+        close(this->dma_heap_fd);
+    }
 }
 
 int Allocator::dmabuf_heap_open(const std::string &path) {
